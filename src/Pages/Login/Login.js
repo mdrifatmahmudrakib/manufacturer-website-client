24999957b2718bbb388dd95ac603df9d76da27bb
Rifat Mahmud <mdrifatmahmudrakib@gmail.com>
2022-05-28 20:37:04 +0600

    Login Page Updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -132,7 +132,7 @@ const Login = () => {
 
                         <input className='btn  w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
-                    <p><small>New To Doctors Portal <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                    <p><small>New To MF Gimbal <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
 
                     <div className="divider">OR</div>
                     <button onClick={() => signInWithGoogle()} className="btn btn-outline btn-accent">Continue With Google</button>
